2f40f0d2963198b05cc1048d990c7af353a564c4
jiremv <paulrivera34@gmail.com>
2025-08-02 21:16:01 -0600

    AWS Connect

--- src/main/java/com/connect/GreetingHandler.java
+++ src/main/java/com/connect/GreetingHandler.java
@@ -2,23 +2,30 @@ package com.connect;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
-import com.squareup.moshi.JsonAdapter;
 import com.squareup.moshi.Moshi;
 import jakarta.enterprise.context.ApplicationScoped;
 
-import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 @ApplicationScoped
-public class GreetingHandler implements RequestHandler<GreetingRequest, GreetingResponse> {
-
-    private final Moshi moshi = new Moshi.Builder().build();
-    private final JsonAdapter<GreetingRequest> requestAdapter = moshi.adapter(GreetingRequest.class);
-
+public class GreetingHandler implements RequestHandler<GreetingRequest, Map<String, Object>> {
     @Override
-    public GreetingResponse handleRequest(GreetingRequest input, Context context) {
+    public Map<String, Object> handleRequest(GreetingRequest input, Context context) {
         context.getLogger().log("Entrada recibida: " + input);
 
         String mensaje = "Hola, " + input.getNombre() + "! Recibimos tu mensaje: '" + input.getMensaje() + "'";
-        return new GreetingResponse(mensaje, "OK");
+        Map<String, String> respuesta = new HashMap<>();
+        respuesta.put("resultado", mensaje);
+        respuesta.put("estado", "OK");
+
+        Map<String, Object> response = new HashMap<>();
+        response.put("statusCode", 200);
+        response.put("headers", Map.of(
+                "Access-Control-Allow-Origin", "*",
+                "Content-Type", "application/json"
+        ));
+        response.put("body", new Moshi.Builder().build().adapter(Map.class).toJson(respuesta));
+        return response;
     }
 }
